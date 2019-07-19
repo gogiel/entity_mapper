4b3d5152b12cff83520b1195541e99be66ead694
Jan Jedrychowski <jan@jedrychowski.org>
2019-07-19 22:24:37 +0200

    Eager load in tests

--- lib/entity_mapper.rb
+++ lib/entity_mapper.rb
@@ -6,6 +6,7 @@ require_relative "entity_mapper/zeitwerk_inflector"
 loader = Zeitwerk::Loader.for_gem
 loader.inflector = EntityMapper::ZeitwerkInfelctor.new(__FILE__)
 loader.setup
+loader.eager_load_exclusions.add File.join(__dir__, "entity_mapper/zeitwerk_inflector.rb")
 
 module EntityMapper
   def self.map
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,8 @@ end
 
 require "rspec/rails"
 
+Zeitwerk::Loader.eager_load_all
+
 RSpec.configure do |config|
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
